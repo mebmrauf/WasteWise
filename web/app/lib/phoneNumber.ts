49cab7fc7574f4550589504baceb06a7223d35d8
Azmari Sultana <126744221+AzmariSultana@users.noreply.github.com>
2026-08-07 21:35:13 +0600

    fix: remove accidentally duplicated lib directory from app folder that caused build error

--- web/app/lib/phoneNumber.ts
+++
@@ -1,6 +0,0 @@
-const BD_PHONE_REGEX = /^(?:\+?880|0)1[3-9]\d{8}$/;
-
-export function isValidBangladeshiPhoneNumber(value: string): boolean {
-  const normalized = value.replace(/[\s-]/g, "");
-  return BD_PHONE_REGEX.test(normalized);
-}
--- web/app/lib/rewards.ts
+++
@@ -1,7 +0,0 @@
-export const POINTS_PER_TAKA = 10;
-
-export const MIN_RECHARGE_TAKA = 20;
-
-export function calculatePointsForRecharge(amountTaka: number): number {
-  return amountTaka * POINTS_PER_TAKA;
-}
